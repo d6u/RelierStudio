feae179c1f1e682c8d4e2c40bdefcc5619d5254c
Daiwei Lu <daiweilu123@gmail.com>
2024-08-18 16:00:52 -0700

    Update

--- packages/canvas-data-base/src/field-definition-types/NodeConfigSubroutineStartSelectFieldDefinition.ts
+++
@@ -1,9 +0,0 @@
-import { type NodeConfigFieldType } from './NodeConfigFieldType';
-
-export type NodeConfigSubroutineStartSelectFieldDefinition<_T = never> = {
-  type: NodeConfigFieldType.SubroutineStartSelect;
-};
-
-export type NodeConfigSubroutineStartSelectFieldConfig = {
-  nodeId: string | null;
-};
--- packages/canvas-data-node-definitions/src/mustache-text-template/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './node-definition';
-export * from './functions';
